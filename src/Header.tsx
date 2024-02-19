3e13e2c053cf4568d64040a289775702a970422b
MariannaBespalova <mariannabespalova9@gmail.com>
2024-02-19 22:33:08 +0100

    first commit

---
+++ src/Header.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const Header = () => {
+  return (
+      <header className='header'>
+        <img src="https://www.pngwing.com/en/free-png-ynlrv" alt=""/>
+      </header>
+  )
+}
